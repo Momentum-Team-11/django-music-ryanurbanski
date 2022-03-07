5154fcc3bd91f1ea65dcf33bcd98367957dc0ffd
Ryan Urbanski <ryanurbanski9@gmail.com>
2022-03-07 00:55:15 -0500

    Album list view working.

--- albums/views.py
+++ albums/views.py
@@ -1,3 +1,8 @@
+from urllib import request
 from django.shortcuts import render
 
-# Create your views here.
+from .models import Albums
+
+def list(request):
+    all_albums = Albums.objects.all()
+    return render(request, 'albums/albums_list.html', {'albums': all_albums})
