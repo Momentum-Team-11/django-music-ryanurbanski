6cdc22a8d64753e4985d27a1018220ae0c12d735
Ryan Urbanski <ryanurbanski9@gmail.com>
2022-03-07 01:05:56 -0500

    Album detail url endpoints added.

--- albums/urls.py
+++ albums/urls.py
@@ -3,5 +3,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('albums', views.list, )
+    path('albums', views.list),
+    path('albums/<int:pk>/', views.detail)
 ]
--- albums/views.py
+++ albums/views.py
@@ -1,3 +1,4 @@
+import re
 from urllib import request
 from django.shortcuts import render
 
@@ -6,3 +7,7 @@ from .models import Albums
 def list(request):
     all_albums = Albums.objects.all()
     return render(request, 'albums/albums_list.html', {'albums': all_albums})
+
+def detail(request, pk):
+    album = Albums.objects.get(pk=pk)
+    return render(request, 'albums/albums_detail.html', {'album': album})
